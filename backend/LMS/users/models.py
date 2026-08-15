dd1032ba0797b6b04dbd89e7f138d4ee3ab53516
Rich-T-Biscuit <richard.taylor656@googlemail.com>
2026-08-15 13:56:13 +0100

    Django Database Update
    Database created

---
+++ backend/LMS/users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT = "STUDENT", "Student"
+        TEACHER = "TEACHER", "Teacher"
+        ADMIN = "ADMIN", "Admin"
+
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.STUDENT,
+    )
